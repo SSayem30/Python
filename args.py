0e3471505c4404ec6600216154819dca1de9540a
SifatSayem <shifatsayem2003@gmail.com>
2026-08-27 19:59:20 +0600

    added dictionary practice

--- args.py
+++ args.py
@@ -1,12 +1,6 @@
-#def greet(dept,name="Sayem"):
-    #print(f"My name is {name}")
-    #print(f"I am from {dept} department.")
+def greet(dept,name="Sayem"):
+    print(f"My name is {name}")
+    print(f"I am from {dept} department.")
 
-#greet(dept="IRE")
-def add(*numbers):
-    c='0'
-    for i in numbers:
-        c=c+i
-    print(f"Sum is {c}")
-
-add(' sifat',' mazumder')        
+greet(dept="IRE")
+  
---
+++ dictioneries.py
@@ -0,0 +1,15 @@
+info={'Goku':'Ultra Instinct',
+      'Luffy':'Gear 5',
+      'Ichigo':'Blood Chain'}
+print(info)
+info['Naruto']={'Sage Mode','Baryon Mode'}
+info['Asta']='Demon Form'
+print(info)
+print(info['Ichigo'])
+print(info.keys())
+print(info.values())
+print(info.items())
+del info['Asta']
+print(info.pop('Naruto'))
+info['Goku']='Mastered Ultra Instinct'
+print(info['Goku'])
