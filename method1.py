46af50d8f1ed6fc85a810b1dcf1d0c2c1e324ba0
SifatSayem <shifatsayem2003@gmail.com>
2026-09-09 13:22:12 +0600

    practice

---
+++ method1.py
@@ -0,0 +1,14 @@
+class CarDesign:
+    count=0
+    def __init__(self,name,speed):
+        self.name=name
+        self.speed=speed
+    def display(self,specialty):
+        self.specialty=specialty
+        print(f"{self.name} is running at {self.speed} Km/h and its specialty is {specialty}")
+        CarDesign.count+=1
+car1=CarDesign("BMW",200)
+car2=CarDesign("Supra",300)
+car1.display("Luxury")
+car2.display("Sports")
+print(f"There are {CarDesign.count} cars designed")
