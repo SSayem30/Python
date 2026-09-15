4aabc4fca915d67f0871d2dc5e6e8915a7b899b0
SifatSayem <shifatsayem2003@gmail.com>
2026-09-15 22:41:23 +0600

    practice

--- inherit.py
+++ inherit.py
@@ -1,21 +1,21 @@
 class Human:
     num_eyes=2
-    def __init__(self):
+    def __init__(self,arms):
         self.num_legs=2
-        self.num_arms=2
+        self.num_arms=arms
     def display(self):
         self.bro=2
         print("Nice to meet you!!")
 class Male(Human):
-    def __init__(self,name):
-        super().__init__()
+    def __init__(self,name,arm):
+        super().__init__(arm)
         self.name=name
     def display(self,work):
         self.work=work
         super().display()
         print(f"I am {self.name} and i have {self.num_legs} legs, {self.num_arms} arms, {Human.num_eyes} eyes")
         print(f"I am a {work} and i have {self.bro} brothers")
-male1=Male("Thomas Shelby")
+male1=Male("Thomas Shelby",2)
 male1.display("Gangster")
-h1=Human()
+h1=Human(2)
 h1.display()
